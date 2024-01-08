e408a19b93a6983e0bf5594a1eebca1b1fdfdd9e
Tim <malikov.t@gmail.com>
2024-01-08 15:09:29 +0100

    hello world

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-set_1 = {1, 2, 3, 4}
+my_range = range(5)
 
-set_1.add(5)
-
-print(set_1)
-
-set_2 = {2, 4, 6, 8}
-
-set_3 = set_1.intersection(set_2)
-
-list = list(set_3)
-
-print(list)
+print(my_range)
+print(type(my_range))
+print(my_range[-1])
