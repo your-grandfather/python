b0f5828ae22d9ecaf327c1502e52e58ea10ca2f7
Tim <malikov.t@gmail.com>
2024-01-10 21:18:43 +0100

    hello world

--- main.py
+++ main.py
@@ -19,23 +19,32 @@
 # post_with_weekday = create_new_post(initial_post)
 #
 # print(post_with_weekday)
+#
+# def print_number_info(num):
+#     if (num % 2) == 0:
+#         print("Entered number is even")
+#     else:
+#         print("Entered number is odd")
+#
+#
+# def print_square_num(num):
+#     print("Square of the num is", num * num)
+#
+#
+# def process_number(num, callback_fn):
+#     callback_fn(num)
+#
+#
+# entered_num = int(input('Enter any number: '))
+#
+# process_number(entered_num, print_number_info)
+# process_number(entered_num, print_square_num)
 
-def print_number_info(num):
-    if (num % 2) == 0:
-        print("Entered number is even")
-    else:
-        print("Entered number is odd")
+c = 5
 
 
-def print_square_num(num):
-    print("Square of the num is", num * num)
+def my_fn(a, b):
+    print(a, b)
 
 
-def process_number(num, callback_fn):
-    callback_fn(num)
-
-
-entered_num = int(input('Enter any number: '))
-
-process_number(entered_num, print_number_info)
-process_number(entered_num, print_square_num)
+print(dir())
