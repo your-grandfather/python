ce6e8de2dff9421dd52813f2d0037d1d2291aed5
Tim <malikov.t@gmail.com>
2024-01-10 01:03:49 +0100

    hello world

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-from datetime import date
+# from datetime import date
+#
+#
+# def get_weekday():
+#     return date.today().strftime('%A')
+#
+#
+# def create_new_post(post, weekday=get_weekday()):
+#     post_copy = post.copy()
+#     post_copy['created_on_weekday'] = weekday
+#     return post_copy
+#
+#
+# initial_post = {
+#     'id': 243,
+#     'author': 'Bogdan'
+# }
+#
+# post_with_weekday = create_new_post(initial_post)
+#
+# print(post_with_weekday)
+
+def print_number_info(num):
+    if (num % 2) == 0:
+        print("Entered number is even")
+    else:
+        print("Entered number is odd")
 
 
-def get_weekday():
-    return date.today().strftime('%A')
+def print_square_num(num):
+    print("Square of the num is", num * num)
 
 
-def create_new_post(post, weekday=get_weekday()):
-    post_copy = post.copy()
-    post_copy['created_on_weekday'] = weekday
-    return post_copy
+def process_number(num, callback_fn):
+    callback_fn(num)
 
 
-initial_post = {
-    'id': 243,
-    'author': 'Bogdan'
-}
+entered_num = int(input('Enter any number: '))
 
-post_with_weekday = create_new_post(initial_post)
-
-print(post_with_weekday)
+process_number(entered_num, print_number_info)
+process_number(entered_num, print_square_num)
