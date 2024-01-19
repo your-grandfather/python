61a0f0720095678fdb26387d9c0a08869f2d733f
Tim <malikov.t@gmail.com>
2024-01-19 08:16:28 +0100

    44

---
+++ codewars.py
@@ -0,0 +1,14 @@
+# 1
+
+def square_sum(numbers):
+    result = 0
+    for a in numbers:
+        result += a * a
+    return result
+
+
+my_list = [-1, 0, 1]
+
+square_sum(my_list)
+
+print(square_sum(my_list))
