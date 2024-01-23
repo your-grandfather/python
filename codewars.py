00e8f2f789490105bf699e5d1ec42507a0c0a278
Tim <malikov.t@gmail.com>
2024-01-23 13:06:29 +0100

    just another CW kata

--- codewars.py
+++ codewars.py
@@ -85,3 +85,11 @@
 # descending_order(15)
 #
 # 13
+# def high_and_low(numbers):
+#     a = [int(num) for num in numbers.split()]
+#     return f"{max(a)} {min(a)}"
+#
+#
+# print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+#
+# 14
