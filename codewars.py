0737d9b832f4ea9f71519cd805a84943973bde9f
Tim <malikov.t@gmail.com>
2024-01-23 15:27:23 +0100

    just another CW kata

--- codewars.py
+++ codewars.py
@@ -113,4 +113,8 @@
 #
 # 16
 
-def are_you_playing_banjo(name):
+# def are_you_playing_banjo(name):
+#     if (name.lower()[0]) == 'r':
+#         return f"{name} plays banjo"
+#     else:
+#         return f"{name} does not play banjo"
